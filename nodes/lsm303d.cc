6f77b02be795eb944e755dbfb91a6f1d3342623e
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2023-04-26 14:17:50 +0100

    LSM303D does not have a gyroscope
    Cartographer requires 6DOF IMU data: linear accel and angular vel.

---
+++ nodes/lsm303d.cc
@@ -0,0 +1,63 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+#include <lsm303d.hh>
+
+using namespace std::chrono_literals;
+
+class LSM303DPublisher : public rclcpp::Node {
+public:
+  LSM303DPublisher()
+  : Node("lsm303d"), lsm303d(0, std::byte{0x1d}) {
+    lsm303d.control1_wr(std::byte{0b0101'0'1'1'1});
+    lsm303d.control2_wr(std::byte{0b00'000'0'0'0});
+    lsm303d.control3_wr(std::byte{0b0'0'0'0'0'0'0'0});
+    lsm303d.control4_wr(std::byte{0b0'0'0'0'0'0'0'0});
+    lsm303d.control5_wr(std::byte{0b0'11'100'0'0});
+    lsm303d.control6_wr(std::byte{0b0'00'00000});
+    lsm303d.control7_wr(std::byte{0b00'0'0'0'0'00});
+
+    publisher_ = this->create_publisher<sensor_msgs::msg::Imu>("/imu", 10);
+    timer_ = this->create_wall_timer(  // 100 Hz -> 10 ms
+      10ms, std::bind(&LSM303DPublisher::publish, this));
+  }
+
+private:
+  void
+  publish() {
+    auto acceleration = lsm303d.accelerometer_rd();
+
+    msg.header.frame_id = "imu_link";
+    msg.header.stamp = this->now();
+
+    msg.linear_acceleration.x = acceleration[0];
+    msg.linear_acceleration.y = acceleration[1];
+    msg.linear_acceleration.z = acceleration[2];
+
+    // LSM303D doesn't produce an orientation and angular velocity estimate
+    msg.orientation_covariance[0] = -1;
+    msg.angular_velocity_covariance[0] = -1;
+    // A zero covariance matrix will be interpreted as "covariance unknown"
+    msg.linear_acceleration_covariance[0] = 0;
+    msg.linear_acceleration_covariance[4] = 0;
+    msg.linear_acceleration_covariance[8] = 0;
+
+    publisher_->publish(msg);
+  }
+
+  LSM303D lsm303d;
+  sensor_msgs::msg::Imu msg;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr publisher_;
+};
+
+
+auto
+main(int argc, char * argv[]) -> int {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LSM303DPublisher>());
+  rclcpp::shutdown();
+}
--- slam.py
+++ slam.py
@@ -21,11 +21,29 @@ def generate_launch_description():
     #     ],
     # )
 
+    # static_tf = Node(
+    #     package = "tf2_ros", 
+    #     executable = "static_transform_publisher",
+    #     arguments = ["0", "0", "0", "0", "0", "0", "odom", "laser"],
+    # )
+
+    base_tf_laser = Node(
+        package = "tf2_ros", 
+        executable = "static_transform_publisher",
+        arguments = ["0", "0", "0", "0", "0", "0", "base_link", "laser_link"],
+    )
+
+    laser_tf_imu = Node(
+        package = "tf2_ros", 
+        executable = "static_transform_publisher",
+        arguments = ["-0.048", "-0.036", "-0.025", "0", "0", "0", "laser_link", "imu_link"],
+    )
+
     hls_lfcd_lds_publisher_node = Node(
         package="hls_lfcd_lds_driver",
         executable="hlds_laser_publisher",
         name="hlds_laser_publisher",
-        parameters=[{"port": "/dev/ttyUSB0", "frame_id": "laser"}],
+        parameters=[{"port": "/dev/ttyUSB0", "frame_id": "laser_link"}],
         output="screen"
     )
 
@@ -52,7 +70,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         # robot_state_publisher_node,
+        base_tf_laser,
+        laser_tf_imu,
         hls_lfcd_lds_publisher_node,
-        # cartographer_node,
-        # cartographer_occupancy_grid_node,
+        cartographer_node,
+        cartographer_occupancy_grid_node,
     ])
